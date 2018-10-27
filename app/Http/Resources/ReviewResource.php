1c98161c24e69a5d1227cdb723241e5ac562477f
Stanley Ezonkeme <dev.staunty@gmail.com>
2018-10-27 20:27:35 +0100

    Workspace

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -16,6 +16,7 @@ class ReviewResource extends Resource
     {
         return [
 
+            'id' => $this->id,
             'customer' => $this->customer,
             'body' => $this->review,
             'star' => $this->star
