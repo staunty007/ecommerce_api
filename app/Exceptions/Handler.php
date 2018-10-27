4340b7dc12fdb246ee5023c69038dd386214c5d3
Stanley Ezonkeme <dev.staunty@gmail.com>
2018-10-27 19:17:21 +0100

    Handlers and Custom Changes to Traits Exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,15 @@
 
 namespace App\Exceptions;
 
+use App\Exceptions\ExceptionTrait;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+
+
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -48,6 +52,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->expectsJson()) {
+
+            return $this->apiException($request,$exception);
+        }
+
+
+        //dd($exception);
+
         return parent::render($request, $exception);
     }
 }
